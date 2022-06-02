926ced3605efe0d0d965874c5c095712c67b8a7c
adayevkp <adayevkp@gmail.com>
2022-06-03 01:23:24 +0300

    add bargaining gap experiment

--- src/experiments/identical_goods/main.py
+++ src/experiments/identical_goods/main.py
@@ -1,8 +1,10 @@
 from src.experiments.identical_goods.compare_guarantees import compare_guarantees
+from src.experiments.identical_goods.bargaining_gap import check_bargaining_gap
 
 
 def run():
     compare_guarantees()
+    check_bargaining_gap()
 
 
 if __name__ == '__main__':
